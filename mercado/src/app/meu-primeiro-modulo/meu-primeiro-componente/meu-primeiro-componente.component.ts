dfcde4a6bf406b65867b3def17f7023fefd9ced3
Arthur Martins Alves <oarthurma@gmail.com>
2025-09-14 07:36:56 -0300

    Trafegando dados entre template e componentes

--- mercado/src/app/meu-primeiro-modulo/meu-primeiro-componente/meu-primeiro-componente.component.ts
+++ mercado/src/app/meu-primeiro-modulo/meu-primeiro-componente/meu-primeiro-componente.component.ts
@@ -3,8 +3,17 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-meu-primeiro-componente',
   templateUrl: './meu-primeiro-componente.component.html',
-  styleUrls: ['./meu-primeiro-componente.component.css']
+  styleUrls: ['./meu-primeiro-componente.component.css'],
 })
 export class MeuPrimeiroComponenteComponent {
+     
+  public  year = 2023;
 
+  acrescentarAno() {
+    this.year++;
+  }
+
+  decrescerAno() {
+    this.year--;
+  }  
 }
